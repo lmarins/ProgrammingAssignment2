501924d43c7d0adffc1fca2b625ce4cfc0191d31
lmarins <alonso.marin@gmail.com>
2022-04-15 23:35:56 -0600

    LAMS v 1.2
    Update LAMS v 1.2

--- Matriz Inversa v1.0.R
+++ Matriz Inversa v1.0.R
@@ -19,3 +19,31 @@ inv(myInvMatrix)
 
 #To transpose a matrix in R, use the t() method.
 tm <- t(myInvMatrix)
+
+#------------------------------------------------------------------------------------------------------
+# R program to illustrate
+# solve function
+  
+# Create 3 different vectors 
+# using combine method. 
+a1 <- c(3, 2, 5) 
+a2 <- c(2, 3, 2) 
+a3 <- c(5, 2, 4) 
+    
+# bind the three vectors into a matrix  
+# using rbind() which is basically 
+# row-wise binding
+A <- rbind(a1, a2, a3) 
+    
+# print the original matrix 
+print(A) 
+    
+# Use the solve() function  
+# to calculate the inverse
+T1 <- solve(A) 
+    
+# print the inverse of the matrix
+print(T1) 
+
+T2 <- t(T1)
+inv(T2)
--- cachematrix.R
+++ cachematrix.R
@@ -2,24 +2,53 @@
 # last update: 20220415.2244
 # ------------------------------------------------------------------------------
 
-## Write a short comment describing this function
-
 ##----------------------------------------------------------------------------------------------------------------- # nolint
-##-- makeCacheMatrix
+## makeCacheMatrix
+## 
 ##----------------------------------------------------------------------------------------------------------------- # nolint
 makeCacheMatrix <- function(x = matrix()) {
-
-
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setSolve <- function(solve) m <<- solve 
+        getSolve <- function() m
+        list(
+                set = set,
+                get = get,
+                setSolve = setSolve,
+                getSolve = getSolve
+        )        
 }
 
-
 ##----------------------------------------------------------------------------------------------------------------- # nolint
-##-- cacheSolve
+## cacheSolve
 ## Return a matrix that is the inverse of 'x' ## nolint
+## solve: This generic function solves the equation a %*% x = b for x, where b can be either a vector or a matrix.
 ##----------------------------------------------------------------------------------------------------------------- # nolint
 cacheSolve <- function(x, ...) { 
-        x <- matrix()
-
-
+        m <- x$getSolve()
+        if(!is.null(m)) {
+                message("Getting cached Data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setSolve(m)
+        m
 }
+
+
+cc1 <- c(1,3,5) 
+cc2 <- c(4,6,9) 
+cc3 <- c(2,9,1) 
+
+cc <- rbind(cc1, cc2, cc3)
+cc
+
+myMatrix <- makeCacheMatrix(cc)
+myMatrix$getSolve()
+myMatrix$set(cc)
+cacheSolve(myMatrix)
